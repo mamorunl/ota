1fd49d889734788920fc90593b5a8acd821a997c
mamoru <info@mamoru.nl>
2015-06-13 20:19:28 +0200

    ServiceProvider set up

--- src/OTAServiceProvider.php
+++ src/OTAServiceProvider.php
@@ -8,6 +8,44 @@
 namespace mamorunl\OTA;
 
 
-class OTAServiceProvider {
+use Illuminate\Routing\Router;
+use Illuminate\Support\ServiceProvider;
 
+class OTAServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    public function boot()
+    {
+        $this->setupViews();
+
+        $this->setupTranslations();
+
+        $this->setupRoutes($this->app->router);
+    }
+
+    protected function setupViews()
+    {
+        $this->loadViewsFrom(realpath(__DIR__ . '/resources/views'), 'mamorunl-ota');
+    }
+
+    protected function setupTranslations()
+    {
+        $this->loadTranslationsFrom(realpath(__DIR__ . '/resources/lang'), 'mamorunl-ota');
+    }
+
+    protected function setupRoutes(Router $router)
+    {
+        $router->group(['namespace' => 'mamorunl\OTA\Http\Controllers'], function (Router $router) {
+            require __DIR__ . '/Http/routes.php';
+        });
+    }
 }
