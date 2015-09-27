18d76de9fdd03467e2886d3ae49ed338f43623a3
mamoru <info@mamoru.nl>
2015-09-27 14:44:49 +0200

    Removed 'use' that was not used

--- src/Http/Controllers/OTAFlightController.php
+++ src/Http/Controllers/OTAFlightController.php
@@ -14,7 +14,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Redirect;
-use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Validator;
 use mamorunl\OTA\Facades\DataToOTAFormatter;
 use mamorunl\OTA\Facades\OTA;
